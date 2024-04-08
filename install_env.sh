cf6849f9ad33ffb98aa329d147c54d857726fa02
Javier Muñoz <javier.munnozharo@estudiante.uam.es>
2024-04-08 12:53:20 +0200

    Created instalation script

---
+++ install_env.sh
@@ -0,0 +1,10 @@
+conda create --name rl_env_tfm -y python=3.11.8
+source activate rl_env_tfm
+
+pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
+pip install gym-super-mario-bros==7.4.0
+pip install tensordict==0.3.0
+pip install torchrl==0.3.0
+
+pip install matplotlib
+
