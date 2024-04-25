41966cb2bf10471623ee595a3ab6908e123cba37
Javier Muñoz <javier.munnozharo@estudiante.uam.es>
2024-04-25 22:29:00 +0200

    Now we can log the progress of the agent saving checkpoints, plotting graphs and saving videos

--- src/train.py
+++ src/train.py
@@ -6,10 +6,13 @@ from utils.wrappers import SkipFrame, GrayScaleObservation, ResizeObservation
 import gymnasium as gym
 import argparse
 import torch
+from pathlib import Path
+import datetime
 
 if __name__ == '__main__':
     
     device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print(f"Using torch with {device}")
     
     parser = argparse.ArgumentParser()
     parser.add_argument("-e", "--environment", help='The name of environment where the agent is going to perform.', default="ALE/DemonAttack-v5")
@@ -18,8 +21,11 @@ if __name__ == '__main__':
     parser.add_argument("-k", "--skip_frames", help='Tells the number of frames to skip and stack for the observation.', default=4)
     parser.add_argument("-b", "--batch_size", help='batch size for the experience replay learning of the DQN.', default=32)
     parser.add_argument("-f", "--discount_factor", help="Discount factor for Q-learning updates", default=0.9)
-    parser.add_argument("-n", "--number_episodes", help="Number of episodes", default=4000)
-    parser.add_argument("-t", "--type", help="Which type of Tranformer to use for vision", default="patch_transfomer")
+    parser.add_argument("-n", "--number_episodes", help="Number of episodes", default=40000)
+    parser.add_argument("-t", "--type", help="Which type of Tranformer to use for vision", default="patch_transformer")
+    parser.add_argument("--save_check_dir", help="Path to the folder where checkpoints are stored", default="../checkpoints")
+    parser.add_argument("--save_video_dir", help="Path to the folder where videos of the agent playing are stored", default="../videos")
+    parser.add_argument("--save_net_every", help="How many steps between saving the network", default=5e5)
     # process the arguments, store them in args
     args = parser.parse_args()
     # create the environment and add the wrappers
@@ -33,15 +39,25 @@ if __name__ == '__main__':
     obs_shape = env.observation_space.shape
     n_actions = env.action_space.n
     
+    save_check_dir = Path(args.save_check_dir) / args.environment / datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+    save_video_dir = Path(args.save_video_dir) / args.environment / datetime.datetime.now().strftime("%Y-%m-%dT%H-%M-%S")
+    
     agent = DQNAgent(type=args.type,
                      obs_shape=obs_shape,
                      action_dim=n_actions,
                      device=device,
                      batch_size=args.batch_size,
-                     gamma=args.discount_factor
+                     gamma=args.discount_factor,
+                     save_every=args.save_net_every,
+                     save_net_dir=save_check_dir
                     )
     
-    trainer = Trainer(env, agent, args.number_episodes, 2)
+    trainer = Trainer(env=env, 
+                      agent=agent, 
+                      n_episodes=args.number_episodes, 
+                      log_every=2,
+                      save_check_dir=save_check_dir,
+                      save_video_dir=save_video_dir)
     
     trainer.train()
     
--- src/trainer.py
+++ src/trainer.py
@@ -1,51 +1,72 @@
 import gymnasium as gym
-from PIL import Image
-import cv2
 from gym.utils.save_video import save_video
 
+from utils.logger import MetricLogger
+import datetime
+
 class Trainer:
-    def __init__(self, env: gym.Env, agent, n_episodes, print_every=200) -> None:
+    def __init__(self, 
+                 env: gym.Env, 
+                 agent, 
+                 n_episodes: int, 
+                 log_every=200, 
+                 save_check_dir="../checkpoint",
+                 save_video_dir='../video'):
         self.env = env
         self.agent = agent
         self.n_episodes = n_episodes
         self.curr_step = 0
-        self.print_every = print_every
-        # TODO: Add attributes for logging
+        self.log_every = log_every
+        self.save_check_dir = save_check_dir
+        self.save_video_dir = save_video_dir
         
     def train(self):
         step = 0
-        ep_length = 0
+        # create if not exists
+        self.save_check_dir.mkdir(parents=True)
+        self.save_video_dir.mkdir(parents=True)
+        logger = MetricLogger(self.save_check_dir)
+        
+        
         for e in range(self.n_episodes):
             # reset environment
             done, trunc = False, False
             state = self.env.reset()
+            measure_array = []
             while (not done) and (not trunc):
                 # 1. get action for state
-                action = self.agent.perform_action(state)
+                start = datetime.datetime.now()
+                action = self.agent.perform_action(state) # 20.69 ms
                 # 2. run action in environment
-                next_state, reward, done, trunc, info = self.env.step(action)
+                next_state, reward, done, trunc, _ = self.env.step(action) # 1.70 ms
                 # 3. collect experience in exp. replay buffer for Q-learning
-                self.agent.store_transition(state, action, reward, next_state, done, trunc)
+                self.agent.store_transition(state, action, reward, next_state, done, trunc) # 0.56 ms
                 # 4. Learn from collected experiences
-                q, loss = self.agent.learn(step)
+                q, loss = self.agent.learn(step) # 39.76 ms
                 # 5. Update the current state 
                 state = next_state
                 # 6. Update step value 
                 step += 1
-            self.curr_step += 1
-            
-            save_video(
-                self.env.render(),
-                "videos",
-                fps=self.env.metadata["render_fps"],
-                episode_index=e
-            )
+                measure = datetime.datetime.now() - start
+                measure = measure.total_seconds() * 1000                
+                # Logging
+                logger.log_step(reward, loss, q)
+                measure_array.append(measure)
                 
-            ep_length = step - ep_length
-            # TODO: It would be nice that, each N episodes, some information was printed out
-            # or plotted in a log and a graph, for better understanding of the decisions
-            if not (e % self.print_every):
-                print(f"INFO | Episode: {e} | Current Loss: {loss} | Mean Q_values: {q} | Progress: {info} | Reward: {reward}")
+            avg_measure = sum(measure_array)/len(measure_array)
+            print(f"Avg. step time for measure: {avg_measure:.2f} ms")
+            
+            if e > 0: 
+                save_video(
+                    self.env.render(),
+                    self.save_video_dir,
+                    fps=self.env.metadata["render_fps"],
+                    episode_index=e
+                )
+                
+            logger.log_episode()
+            if (not(e % self.log_every) and e > 0) or (e == self.n_episodes - 1) :
+                logger.record(episode=e, epsilon=self.agent.gamma, step=step)
                 
             
         
