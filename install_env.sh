4a7c537df55c55116ceff69c14bae8f09fe9d0ed
Javier Muñoz <javier.munnozharo@estudiante.uam.es>
2024-04-27 18:35:38 +0200

    Changed the commands from pip to pip3 (bets practices)

--- install_env.sh
+++ install_env.sh
@@ -2,12 +2,12 @@ conda create --name rl_env_tfm -y python=3.11.8
 source activate rl_env_tfm
 
 pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118
-pip install gym-super-mario-bros==7.4.0
-pip install tensordict==0.3.0
-pip install torchrl==0.3.0
+pip3 install gym-super-mario-bros==7.4.0
+pip3 install tensordict==0.3.0
+pip3 install torchrl==0.3.0
 
-pip install matplotlib
+pip3 install matplotlib
 
-pip install gymnasium[accept-rom-license]
-pip install gymnasium[atari]
-pip install timm
+pip3 install gymnasium[accept-rom-license]
+pip3 install gymnasium[atari]
+pip3 install timm
