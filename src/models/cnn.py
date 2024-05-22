ebd5d322c6a7357af47c4d7abd00004e42efeb22
Javier Muñoz Haro <javier.munnozharo@estudiante.uam.es>
2024-05-22 18:20:10 +0200

    Added baseline cnn model from silver's paper

---
+++ src/models/cnn.py
@@ -0,0 +1,21 @@
+import torch
+from torch import nn
+
+class CNN(nn.Module):
+    def __init__(self, n_actions:int):
+        super().__init__()
+        self.network = nn.Sequential(
+            nn.Conv2d(4, 32, 8, stride=4),
+            nn.ReLU(),
+            nn.Conv2d(32, 64, 4, stride=2),
+            nn.ReLU(),
+            nn.Conv2d(64, 64, 3, stride=1),
+            nn.ReLU(),
+            nn.Flatten(),
+            nn.Linear(3136, 512),
+            nn.ReLU(),
+            nn.Linear(512, n_actions),
+        )
+
+    def forward(self, x):
+        return self.network(x)
