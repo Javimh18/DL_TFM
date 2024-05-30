8c83ff52aae740409a8a3d653daec9a517eb6605
Javier Muñoz Haro <javier.munnozharo@estudiante.uam.es>
2024-05-30 19:58:31 +0200

    Added new packages to installation

--- install_env.sh
+++ install_env.sh
@@ -14,5 +14,7 @@ pip3 install timm
 pip3 install moviepy
 pip3 install torchsummary
 pip3 install pyyaml
+pip3 install stable_baselines3
+pip3 install pandas
 pip3 install plotly==5.22.0
 pip install -U kaleido
